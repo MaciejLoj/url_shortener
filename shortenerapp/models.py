af9bbc40690f810bbd4e2972a8dfa3cddd3bbf99
Maciej Loj <maciej.loj@gmail.com>
2019-04-08 03:49:08 +0200

    updated models

--- shortenerapp/models.py
+++ shortenerapp/models.py
@@ -16,4 +16,4 @@ class URLShortener(models.Model):
         return str(self.url)
 
     def show_url(self):
-        return "http://127.0.0.1:8000/{short_url}".format(short_url=self.short_url)
+        return "https://shorturlcreator.herokuapp.com/{short_url}".format(short_url=self.short_url)
